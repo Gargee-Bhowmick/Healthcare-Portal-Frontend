96c5f71db850ece85048ad6f6a3bae91be629b4e
Gargee Bhowmick <gargee.bhowmick09@gmail.com>
2025-09-08 01:48:39 +0530

    dealing with cors policy

--- healthcare/src/services/apiClient.js
+++ healthcare/src/services/apiClient.js
@@ -1,7 +1,5 @@
-// apiClient.js
 import axios from "axios";
 
-// Determine backend URL based on environment
 const BASE_URL =
   import.meta.env.VITE_API_URL || "https://hms-b0e0ash9b3fda7ab.southeastasia-01.azurewebsites.net";
 
@@ -10,9 +8,9 @@ const apiClient = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // allow cookies / credentials
 });
 
-// Request interceptor: attach token if exists
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("access_token");
@@ -24,7 +22,6 @@ apiClient.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-// Response interceptor: handle 401
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
@@ -38,56 +35,3 @@ apiClient.interceptors.response.use(
 );
 
 export default apiClient;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// apiClient.js
-// import axios from "axios";
-
-// const apiClient = axios.create({
-//   baseURL: "https://hms-b0e0ash9b3fda7ab.southeastasia-01.azurewebsites.net", // backend URL
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// Request Interceptor: Attach token if available
-// apiClient.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("access_token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
- // Response Interceptor: Handle errors
-// apiClient.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem("access_token"); // clear token
-//       window.dispatchEvent(new Event("unauthorized")); // custom event
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default apiClient;
