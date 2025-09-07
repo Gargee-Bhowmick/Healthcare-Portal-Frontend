2f62a7839f61f5545177a20eb24aca2816aa2728
Gargee Bhowmick <gargee.bhowmick09@gmail.com>
2025-09-07 13:19:58 +0530

    Compliant with fastapi

--- healthcare/src/services/authService.js
+++ healthcare/src/services/authService.js
@@ -1,18 +1,24 @@
 // authService.js
 import apiClient from "./apiClient";
 
-const TOKEN_KEY = "access_token";
-const ROLE_KEY = "user_role";
+// Centralized keys (good practice)
+const TOKEN_KEY = "myapp_access_token";
+const ROLE_KEY = "myapp_user_role";
 
 const authService = {
   register: (userData) => apiClient.post("/register", userData),
 
   login: async ({ username, password }) => {
+    // FastAPI OAuth2 expects x-www-form-urlencoded
     const response = await apiClient.post(
       "/login",
       new URLSearchParams({
+        grant_type: "password", // required by FastAPI spec
         username,
         password,
+        scope: "", // empty unless you use scopes
+        client_id: "", // ignored by FastAPI default
+        client_secret: "", // ignored by FastAPI default
       }),
       { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
     );
